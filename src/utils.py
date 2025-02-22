266a9291e838762507271c2432bef322507483d9
Aleksandr <zarnot91@mail.ru>
2025-02-22 22:22:05 +0500

    Домашнее задание 12.2

--- src/utils.py
+++ src/utils.py
@@ -1,25 +1,34 @@
+import logging
 import os
 import json
 from typing import List, Dict
 
+# Настройка логирования
+logger = logging.getLogger('utils')
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler('logs/utils.log', mode='w', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
 
 def read_transactions(file_path: str) -> List[Dict]:
-    """Читает JSON-файл и возвращает список транзакций."""
     if not os.path.exists(file_path):
-        print(f"Ошибка: файл не найден: {file_path}")
+        logger.error(f"Ошибка: файл не найден: {file_path}")
         return []
 
     try:
         with open(file_path, "r", encoding="utf-8") as file:
-            # Загружаем JSON
             data = json.load(file)
             if isinstance(data, list):
+                logger.debug(f"Прочитано {len(data)} транзакций")
                 return data
             else:
-                print("Ошибка: JSON-файл содержит не список")
+                logger.error("Ошибка: JSON-файл содержит не список")
     except json.JSONDecodeError as e:
-        print(f"Ошибка: Ошибка декодирования JSON: {e}")
+        logger.error(f"Ошибка декодирования JSON: {e}")
     except OSError as e:
-        print(f"Ошибка: Ошибка при чтении файла: {e}")
+        logger.error(f"Ошибка при чтении файла: {e}")
 
     return []
